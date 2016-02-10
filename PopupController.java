410c835ae1694c3bf79ffcc11376eb09cbf08db0
snakebyte72 <laszlo.glant@gmail.com>
2016-02-10 15:25:49 -0500

    Update PopupController.java

--- PopupController.java
+++ PopupController.java
@@ -2,6 +2,7 @@
 
 package SongLibView;
 
+import app.SongLib;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -24,19 +25,21 @@ public class PopupController {
 	@FXML
 	public Button cancelButton, okButton;
 	
+	TextField yearDigits = new TextField();
+		
 	@FXML
 	private void cancelButtonEvent(ActionEvent event) {
 		Stage stage = (Stage) cancelButton.getScene().getWindow();
 		stage.close();
 	}
-
+	
 	@FXML
 	private void okButtonEvent(ActionEvent event) {
 		try {
-			System.out.println("title is " + title.getText());
+			/*System.out.println("title is " + title.getText());
 			System.out.println("artist is " + artist.getText());
 			System.out.println("album is " + album.getText());
-			System.out.println("year is " + year.getText());
+			System.out.println("year is " + year.getText());*/
 			
 			// check if title and artist are empty or not, can't be
 			
@@ -46,8 +49,8 @@ public class PopupController {
 //			Song.output(Song.songList);
 			
 			Boolean didAdd = Song.addInAbcOrder(Song.songList, SongViewController.obsList, s);
-						
-			if (didAdd == true) {
+			//Boolean digit =	isDigit(year);
+			if (didAdd) {
 				// added new song successfully
 				Song.sortAbcStrings(SongViewController.obsList);
 				Song.sortAbcAL(Song.songList);
@@ -56,7 +59,7 @@ public class PopupController {
 				
 			} else {
 				// was duplicate, did not add, open up pop up that was duplicate
-				
+				SongLib.showDuplicatePopupScene();
 			}	
 			
 			Song.printBothLists(Song.songList, SongViewController.obsList);
@@ -70,5 +73,18 @@ public class PopupController {
 		// close pop up window just like cancel does
 		Stage stage = (Stage) okButton.getScene().getWindow();
 		stage.close();
+	}
+	//@FXML
+	private boolean isDigit(TextField year) {
+		if (!(year.getText() == null || year.getText().length() == 0)) {
+			try {
+	            // Do all the validation you need here such as
+	            Integer.parseInt(year.getText());
+	            return true;
+	        }catch (NumberFormatException e) { 
+	        	return false;
+	        }
+		}
+		return false;
 	}
 }
