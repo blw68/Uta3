c73e4b4e5fdec924d7bbec5b3d36245bb6952a29
blw68 <blw68@scarletmail.rutgers.edu>
2016-02-10 23:21:36 -0500

    Update SongLib.java

--- SongLib.java
+++ SongLib.java
@@ -22,6 +22,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextInputDialog;
@@ -31,27 +32,37 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class SongLib extends Application {
-	
+
 	private static Stage primaryStage;
 	private AnchorPane mainLayout;
 	private static AnchorPane addNewSong, editSong, deleteSong, duplicatePopup;
-	
+
 	@Override
 	public void start(Stage primaryStage) throws IOException {
+//		try {
+//			System.out.println("beginning of start");
+//			FXMLLoader loader = new FXMLLoader(getClass().getResource("/SongLibView/SongLibMainScene.fxml"));
+//			System.out.println("before Parent root");
+//			Parent root = (Parent) loader.load();
+//			System.out.println("before controller ");
+//			SongViewController controller = (SongViewController)loader.getController();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		SongLib.primaryStage = primaryStage;
 		SongLib.primaryStage.setTitle("Song Library");
 		showMainView();
 	}
-	
+
 	public void showMainView() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-	    loader.setLocation(getClass().getResource("/SongLibView/SongLibMainScene.fxml"));
+		loader.setLocation(getClass().getResource("/SongLibView/SongLibMainScene.fxml"));
 		mainLayout = loader.load();
 		Scene scene = new Scene(mainLayout);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
+
 	public static void showAddScene() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(SongLib.class.getResource("/SongLibView/PopUpWindow.fxml"));
@@ -64,7 +75,7 @@ public class SongLib extends Application {
 		addWindow.setScene(scene);
 		addWindow.showAndWait();
 	}
-	
+
 	public static void showEditScene() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(SongLib.class.getResource("/SongLibView/PopUpWindow.fxml"));
@@ -77,7 +88,7 @@ public class SongLib extends Application {
 		addWindow.setScene(scene);
 		addWindow.showAndWait();
 	}
-	
+
 	public static void showDeleteScene() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(SongLib.class.getResource("/SongLibView/DeletePopup.fxml"));
