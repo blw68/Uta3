147e91bf194ce58d62e8dbfa7278e471322a2bea
blw68 <blw68@scarletmail.rutgers.edu>
2016-02-10 23:22:07 -0500

    Update SongViewController.java

--- SongViewController.java
+++ SongViewController.java
@@ -4,7 +4,8 @@ package SongLibView;
 
 import java.io.File;
 import java.io.IOException;
-
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import app.SongLib;
 import javafx.event.ActionEvent;
@@ -13,12 +14,13 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class SongViewController {
+public class SongViewController implements Initializable{
 
 	private SongLib songlib;
 	public static int index;
@@ -33,10 +35,10 @@ public class SongViewController {
 	private TextField album;
 	@FXML
 	private TextField year;
-	
-	
-	
-	
+
+
+
+
 	@FXML 
 	public ListView<String> listView;
 
@@ -66,20 +68,36 @@ public class SongViewController {
 		SongLib.showDeleteScene();
 		listView.getSelectionModel().select(index);
 	}
-	
+
 	@FXML
 	public void initialize(){
-		
+		System.out.println("in initialize");
 		// take contents from File f (output.txt) and load into both songList and obsList
 		Song.input(Song.songList, obsList, f);
-				
+
 		// display entries in obsList
 		listView.setItems(obsList);
 		listView.getSelectionModel().select(0);
 		//Song.printList(Song.songList);
 		//Song.printObsList(obsList);
-		
+
+		//artist.setText("Hello");
 		//System.out.println(artist.getText());
 	}
-	
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// take contents from File f (output.txt) and load into both songList and obsList
+		System.out.println("in new initialize");
+		Song.input(Song.songList, obsList, f);
+
+		// display entries in obsList
+		listView.setItems(obsList);
+		listView.getSelectionModel().select(0);
+
+		artist.setText("Hello");
+		album.setText("Album");
+		year.setText("1234");
+	}
+
 }
