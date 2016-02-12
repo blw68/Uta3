664347563314e1cbbfd6c997db189bc6178d1cc5
blw68 <blw68@scarletmail.rutgers.edu>
2016-02-12 00:48:57 -0500

    Update SongViewController.java

--- SongViewController.java
+++ SongViewController.java
@@ -18,27 +18,42 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class SongViewController implements Initializable{
 
 	private SongLib songlib;
-	public static int index;
+	public static int index=0;
 	File f = new File("output.txt");
 
 
-	
+
 	@FXML
 	private TextField artist;
 	@FXML
 	private TextField album;
 	@FXML
 	private TextField year;
-	
+
 	@FXML 
 	public ListView<String> listView;
 
 	public static ObservableList<String> obsList = FXCollections.observableArrayList();
+
+	@FXML public void handleMouseClick(MouseEvent arg0) {
+		if (Song.songList.isEmpty() == true) {
+			artist.setText("");
+			album.setText("");
+			year.setText("");
+			return;
+		}
+		index = listView.getSelectionModel().getSelectedIndex();
+		artist.setText(Song.songList.get(index).artist);
+		album.setText(Song.songList.get(index).album);
+		year.setText(Song.songList.get(index).year+"");
+		System.out.println("clicked on " + listView.getSelectionModel().getSelectedItem());
+	}
 
 	@FXML
 	private void homeScene() throws IOException {
@@ -54,6 +69,7 @@ public class SongViewController implements Initializable{
 	@FXML
 	private void editButton() throws IOException {
 		index = listView.getSelectionModel().getSelectedIndex();
+		//EditController.init();
 		SongLib.showEditScene();
 		if(Song.songList.size() == 0){
 			return;
@@ -69,10 +85,10 @@ public class SongViewController implements Initializable{
 		}
 		SongLib.showDeleteScene();
 		listView.getSelectionModel().select(index);
-		
+
 	}
-		
-	
+
+
 	@FXML
 	public void initialize(){
 		System.out.println("in initialize");
@@ -97,10 +113,22 @@ public class SongViewController implements Initializable{
 		// display entries in obsList
 		listView.setItems(obsList);
 		listView.getSelectionModel().select(0);
+		if (Song.songList.isEmpty() == true) {
+			return;
+		} else {
 
-		artist.setText("Hello");
-		album.setText("Album");
-		year.setText("1234");
+			try {
+				System.out.println("index " + index);
+				artist.setText(Song.songList.get(index).artist);
+				album.setText(Song.songList.get(index).album);
+				year.setText(Song.songList.get(index).year+"");
+			} catch (Exception e) {
+				e.printStackTrace();;
+			}
+		}
+		//		artist.setText("Hello");
+		//		album.setText("Album");
+		//		year.setText("1234");
 	}
-	
+
 }
